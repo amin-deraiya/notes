56b25b2dcd7f4375e98f2ae5b761e450956e10cc
Amin <amin.deraiya.9@gmail.com>
2024-06-20 22:40:30 +0530

    crypto-js

--- app/lib/decodeText.ts
+++ app/lib/decodeText.ts
@@ -1,32 +1,17 @@
-const secretKey: string = process.env.SECRET_KEY || '';
+const CryptoJS = require('crypto-js');
 
-// Function to XOR the data with the secret key
-function xorEncryptDecrypt(data: string, key: string) {
-  let output = [];
-  for (let i = 0; i < data.length; i++) {
-    output.push(data.charCodeAt(i) ^ key.charCodeAt(i % key.length));
-  }
-  return String.fromCharCode.apply(String, output);
-}
-
-// Function to encode to Base64
-function base64Encode(data: string) {
-  return btoa(data);
-}
-
-// Function to decode from Base64
-function base64Decode(data: string) {
-  return atob(data);
-}
+const secretKey = process.env.SECRET_KEY
 
 // Function to encrypt data using a secret key
 export const encode = (data: string) => {
-  const xorData = xorEncryptDecrypt(data, secretKey);
-  return base64Encode(xorData);
+  var ciphertext = CryptoJS.AES.encrypt(data, secretKey).toString();
+  return ciphertext
 };
 
 // Function to decrypt data using a secret key
 export const decode = (data: string) => {
-  const base64Decoded = base64Decode(data);
-  return xorEncryptDecrypt(base64Decoded, secretKey);
+  var bytes  = CryptoJS.AES.decrypt(data, secretKey);
+  var originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+  return originalText
 };
