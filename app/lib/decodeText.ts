078c385cecdaf64dfe99f8bdf18751c709fe2cef
Amin Deraiya <amin.deraiya.9@gmail.com>
2024-06-21 12:35:17 +0530

    added return types in decodeText.ts

--- app/lib/decodeText.ts
+++ app/lib/decodeText.ts
@@ -1,11 +1,15 @@
 const crypto = require('crypto');
 
-const secretKey = process.env.SECRET_KEY
+const secretKey: string = process.env.SECRET_KEY || '';
+
+if (!secretKey) {
+  throw new Error('Secret key is not defined in the environment variables.');
+}
 
 // Function to encrypt data using a secret key
-export const encode = (data) => {
+export const encode = (data: string): string => {
   const iv = crypto.randomBytes(16); // Initialization Vector for AES
-  const cipher = crypto.createCipheriv('aes-256-cbc', secretKey, iv);
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secretKey, 'hex'), iv);
   let encryptedData = cipher.update(data);
   encryptedData = Buffer.concat([encryptedData, cipher.final()]);
 
@@ -13,12 +17,13 @@ export const encode = (data) => {
 };
 
 // Function to decrypt data using a secret key
-export const decode = (data) => {
+export const decode = (data: string): string => {
   const iv = Buffer.from(data.substring(0, 32), 'hex');
   const encryptedData = Buffer.from(data.substring(32), 'hex');
-  const decipher = crypto.createDecipheriv('aes-256-cbc', secretKey, iv);
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secretKey, 'hex'), iv);
   let decryptedData = decipher.update(encryptedData);
   decryptedData = Buffer.concat([decryptedData, decipher.final()]);
 
   return decryptedData.toString('utf8');
 };
+
