e63f199c3b144d9421b7dba691aa583043bc0dc2
Amin Deraiya <amin.deraiya.9@gmail.com>
2024-06-21 11:50:07 +0530

    crypto-js to crypto

--- app/lib/decodeText.ts
+++ app/lib/decodeText.ts
@@ -1,17 +1,24 @@
-const CryptoJS = require('crypto-js');
+const crypto = require('crypto');
 
 const secretKey = process.env.SECRET_KEY
 
 // Function to encrypt data using a secret key
-export const encode = (data: string) => {
-  var ciphertext = CryptoJS.AES.encrypt(data, secretKey).toString();
-  return ciphertext
+const encode = (data) => {
+  const iv = crypto.randomBytes(16); // Initialization Vector for AES
+  const cipher = crypto.createCipheriv('aes-256-cbc', secretKey, iv);
+  let encryptedData = cipher.update(data);
+  encryptedData = Buffer.concat([encryptedData, cipher.final()]);
+
+  return iv.toString('hex') + encryptedData.toString('hex');
 };
 
 // Function to decrypt data using a secret key
-export const decode = (data: string) => {
-  var bytes  = CryptoJS.AES.decrypt(data, secretKey);
-  var originalText = bytes.toString(CryptoJS.enc.Utf8);
+const decode = (data) => {
+  const iv = Buffer.from(data.substring(0, 32), 'hex');
+  const encryptedData = Buffer.from(data.substring(32), 'hex');
+  const decipher = crypto.createDecipheriv('aes-256-cbc', secretKey, iv);
+  let decryptedData = decipher.update(encryptedData);
+  decryptedData = Buffer.concat([decryptedData, decipher.final()]);
 
-  return originalText
+  return decryptedData.toString('utf8');
 };
